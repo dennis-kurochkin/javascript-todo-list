db31a83bb65ecd4a64a4cb9630db0cfd3e1b1d67
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-07-24 13:18:24 +0300

    Finish v4

--- app/app.js
+++ app/app.js
@@ -1,31 +1,45 @@
+// Todo Class
+var Todo = function (text) {
+  this.todoText = text;
+  this.completed = false;
+
+  // Method to toggle the todo completion
+  this.toggleCompleted = function () {
+    this.completed = !this.completed;
+  }
+}
+
 var todos = {
   // Todos data array
   todos: [
-    'item 1',
-    'item 2',
-    'item 3'
+    new Todo('item 1'),
+    new Todo('item 2'),
+    new Todo('item 3')
   ],
   // Method to display todos
   displayTodos: function () {
     console.log('My todos:');
-
-    this.todos.forEach(todo => console.log(todo));
-
+    this.todos.forEach(todo => console.log(todo.completed ? '[x]' : '[ ]', todo.todoText));
     console.log('==========');
   },
   // Method to add todo
-  addTodo: function (todo) {
-    this.todos.push(todo);
+  addTodo: function (todoText) {
+    this.todos.push(new Todo(todoText));
     this.displayTodos();
   },
   // Method to change todo
-  changeTodo: function (position, newValue) {
-    this.todos[position] = newValue;
+  changeTodo: function (position, newTodoText) {
+    this.todos[position].todoText = newTodoText;
     this.displayTodos();
   },
   // Method to delete todo
   deleteTodo: function (position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  // Method to toggle the todo completion
+  toggleCompleted: function (position) {
+    this.todos[position].toggleCompleted();
     this.displayTodos();
   }
 }
@@ -33,4 +47,5 @@ var todos = {
 todos.displayTodos();
 todos.addTodo('item 4');
 todos.changeTodo(2, 'hey there');
-todos.deleteTodo(1);
+todos.toggleCompleted(1);
+todos.deleteTodo(0);
---
+++ v4_requirements.md
@@ -0,0 +1,5 @@
+# v3 Requirements
+
+- [x] todoList.addTodo should add object instead of strings
+- [x] todoList.changeTodo should change the todoText property
+- [x] todoList.toggleCompleted should change the completed property
