755cdd1eae1b873670d4fd22d78bde2a3167f2bf
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-07-24 11:30:17 +0300

    Finish v3

--- app/app.js
+++ app/app.js
@@ -1,39 +1,36 @@
-var todos = [
-  'item 1',
-  'item 2',
-  'item 3'
-];
+var todos = {
+  // Todos data array
+  todos: [
+    'item 1',
+    'item 2',
+    'item 3'
+  ],
+  // Method to display todos
+  displayTodos: function () {
+    console.log('My todos:');
 
-function displayTodos() {
-  console.log('My todos:');
+    this.todos.forEach(todo => console.log(todo));
 
-  todos.forEach(todo => {
-    console.log(todo);
-  })
+    console.log('==========');
+  },
+  // Method to add todo
+  addTodo: function (todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
+  // Method to change todo
+  changeTodo: function (position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+  // Method to delete todo
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
 }
 
-displayTodos();
-
-function addTodo(todo) {
-  todos.push(todo);
-
-  displayTodos();
-}
-
-addTodo('item 4');
-
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-
-  displayTodos();
-}
-
-changeTodo(2, 'hey there');
-
-function deleteTodo(position) {
-  todos.splice(position, 1);
-
-  displayTodos();
-}
-
-deleteTodo(1);
+todos.displayTodos();
+todos.addTodo('item 4');
+todos.changeTodo(2, 'hey there');
+todos.deleteTodo(1);
--- v3_requirements.md
+++ v3_requirements.md
@@ -1,7 +1,7 @@
 # v3 Requirements
 
-- [ ] It should store the todos array on an object
-- [ ] It should have a displayTodos method
-- [ ] It should have an addTodo method
-- [ ] It should have a changeTodo method
-- [ ] It should have a deleteTodo method
+- [x] It should store the todos array on an object
+- [x] It should have a displayTodos method
+- [x] It should have an addTodo method
+- [x] It should have a changeTodo method
+- [x] It should have a deleteTodo method
