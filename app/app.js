c10191772224a96292b1ee5d95e9125d5e4d16ba
Dennis Kurochkin <dennis.kurochkin@gmail.com>
2020-07-31 16:34:56 +0300

    Refactor and make components

--- app/app.js
+++ app/app.js
@@ -16,11 +16,10 @@ var Todo = function (text) {
 
 }
 
-// Main App object
-var App = {
-
+// UI component
+var UI = (function () {
   // DOM Elements
-  DOMElements: {
+  var DOMElements = {
     buttonDisplayTodos: '.js-display-todos',
     buttonToggleAll: '.js-toggle-all',
     addTodoForm: '.js-add-todo-form',
@@ -28,71 +27,27 @@ var App = {
     deleteTodoForm: '.js-delete-todo-form',
     toggleCompletedTodoForm: '.js-toggle-completed-todo-form',
     todoList: '.js-todo-list',
-  },
+  }
 
-  // Todos data array
-  todos: [
-    new Todo('item 1'),
-    new Todo('item 2'),
-    new Todo('item 3')
-  ],
+  return {
+    // Method to get DOM elements
+    getDOMElements: function () {
+      return DOMElements;
+    },
+    // Method to display todos
+    displayTodos: function () {
+      var todoList = document.querySelector(DOMElements.todoList);
+      var todos = App.getTodos();
 
-  // Method to display todos
-  displayTodos: function () {
-    var todoList = document.querySelector(this.DOMElements.todoList);
+      todoList.innerHTML = '';
 
-    todoList.innerHTML = '';
+      todos.forEach(function (todo, index) {
+        var todoElement = document.createElement('li');
 
-    this.todos.forEach(function (todo, index) {
-      var todoElement = document.createElement('li');
-
-      todoElement.innerText = `${index}. [${todo.completed ? 'x' : ' '}] ${todo.todoText}`;
-      todoList.appendChild(todoElement);
-    });
-  },
-
-  // Method to add todo
-  addTodo: function (todoText) {
-    this.todos.push(new Todo(todoText));
-  },
-
-  // Method to change todo
-  changeTodo: function (position, newTodoText) {
-    this.todos[position].todoText = newTodoText;
-  },
-
-  // Method to delete todo
-  deleteTodo: function (position) {
-    this.todos.splice(position, 1);
-  },
-
-  // Method to toggle the todo completion
-  toggleCompleted: function (position) {
-    this.todos[position].toggleCompleted();
-  },
-
-  // Method to toggle all the todos
-  toggleAll: function () {
-    var everyTodoIsCompleted = true;
-
-    // Check if every todo is completed
-    this.todos.forEach(function (todo) {
-      if (!todo.completed) {
-        everyTodoIsCompleted = false;
-      }
-    });
-
-    this.todos.forEach(function (todo) {
-      if (everyTodoIsCompleted) { // if every todo is completed then toggle them all to be uncompleted
-        todo.toggleCompleted();
-      } else { // if there are completed todos in the list then complete all of them
-        todo.complete();
-      }
-    });
-  },
-
-  // Event handlers
-  handlers: {
+        todoElement.innerText = `${index}. [${todo.completed ? 'x' : ' '}] ${todo.todoText}`;
+        todoList.appendChild(todoElement);
+      });
+    },
     // Add todo handler
     addTodoHandler: function (e) {
       e.preventDefault();
@@ -105,90 +60,151 @@ var App = {
         todoTextInput.value = ''; // clear the input
       }
 
-      App.displayTodos();
+      UI.displayTodos();
     },
     // Change todo handler
     changeTodoHandler: function (e) {
       e.preventDefault()
 
+      var todos = App.getTodos();
       var todoPositionInput = this.querySelector('input.js-todo-position');
       var todoTextInput = this.querySelector('input.js-todo-text');
 
       if (
         todoPositionInput.value !== '' && todoTextInput.value !== '' && // check if inputs contain values
-        todoPositionInput.valueAsNumber < App.todos.length // check if input position is valid
+        todoPositionInput.valueAsNumber < todos.length // check if input position is valid
       ) {
         App.changeTodo(todoPositionInput.value, todoTextInput.value); // change todo with the given parameters
         todoPositionInput.value = ''; // clear the inputs
         todoTextInput.value = '';
       }
 
-      App.displayTodos();
+      UI.displayTodos();
     },
     // Delete todo handler
     deleteTodoHandler: function (e) {
       e.preventDefault()
 
+      var todos = App.getTodos();
       var todoPositionInput = this.querySelector('input.js-todo-position');
 
       // Check if the input contains value
-      if (todoPositionInput.value !== '' && todoPositionInput.valueAsNumber < App.todos.length) {
+      if (todoPositionInput.value !== '' && todoPositionInput.valueAsNumber < todos.length) {
         App.deleteTodo(todoPositionInput.value); // change todo with the given parameters
         todoPositionInput.value = ''; // clear the inputs
       }
 
-      App.displayTodos();
+      UI.displayTodos();
     },
     // Toggle completed handler
     toggleCompletedHandler: function (e) {
       e.preventDefault();
 
+      var todos = App.getTodos();
       var todoPositionInput = this.querySelector('input.js-todo-position');
 
       // Check if the input contains value
-      if (todoPositionInput.value !== '' && todoPositionInput.valueAsNumber < App.todos.length) {
+      if (todoPositionInput.value !== '' && todoPositionInput.valueAsNumber < todos.length) {
         App.toggleCompleted(todoPositionInput.value); // toggle todo
         todoPositionInput.value = ''; // clear the input
       }
 
-      App.displayTodos();
+      UI.displayTodos();
     },
     // Toggle all handler
     toggleAllHandler: function () {
       App.toggleAll();
-      App.displayTodos();
+      UI.displayTodos();
     }
-  },
-
-  // Init method
-  init: function () {
-    var self = this;
-
-    // Display initial todos
-    this.displayTodos();
-
-    // Add event listener for the add todo form
-    document.querySelector(this.DOMElements.addTodoForm)
-      .addEventListener('submit', this.handlers.addTodoHandler);
-
-    // Add event listener for the change todo form
-    document.querySelector(this.DOMElements.changeTodoForm)
-      .addEventListener('submit', this.handlers.changeTodoHandler);
-
-    // Add event listener for the delete todo form
-    document.querySelector(this.DOMElements.deleteTodoForm)
-      .addEventListener('submit', this.handlers.deleteTodoHandler);
-
-    // Add event listener for the toggle completed todo form
-    document.querySelector(this.DOMElements.toggleCompletedTodoForm)
-      .addEventListener('submit', this.handlers.toggleCompletedHandler)
-
-    // Add event listener for the toggle all button
-    document.querySelector(this.DOMElements.buttonToggleAll)
-      .addEventListener('click', this.handlers.toggleAllHandler);
   }
+})()
 
-}
+// Main App object
+var App = (function () {
+
+  // Todos data array
+  var todos = [
+    new Todo('item 1'),
+    new Todo('item 2'),
+    new Todo('item 3')
+  ];
+
+  return {
+    // Get todos array
+    getTodos: function () {
+      return todos;
+    },
+
+    // Method to add todo
+    addTodo: function (todoText) {
+      todos.push(new Todo(todoText));
+    },
+
+    // Method to change todo
+    changeTodo: function (position, newTodoText) {
+      todos[position].todoText = newTodoText;
+    },
+
+    // Method to delete todo
+    deleteTodo: function (position) {
+      todos.splice(position, 1);
+    },
+
+    // Method to toggle the todo completion
+    toggleCompleted: function (position) {
+      todos[position].toggleCompleted();
+    },
+
+    // Method to toggle all the todos
+    toggleAll: function () {
+      var everyTodoIsCompleted = true;
+
+      // Check if every todo is completed
+      todos.forEach(function (todo) {
+        if (!todo.completed) {
+          everyTodoIsCompleted = false;
+        }
+      });
+
+      todos.forEach(function (todo) {
+        if (everyTodoIsCompleted) { // if every todo is completed then toggle them all to be uncompleted
+          todo.toggleCompleted();
+        } else { // if there are completed todos in the list then complete all of them
+          todo.complete();
+        }
+      });
+    },
+
+    // Init method
+    init: function () {
+      var DOMElements = UI.getDOMElements();
+
+      // Display initial todos
+      UI.displayTodos();
+
+      // Add event listener for the add todo form
+      document.querySelector(DOMElements.addTodoForm)
+        .addEventListener('submit', UI.addTodoHandler);
+
+      // Add event listener for the change todo form
+      document.querySelector(DOMElements.changeTodoForm)
+        .addEventListener('submit', UI.changeTodoHandler);
+
+      // Add event listener for the delete todo form
+      document.querySelector(DOMElements.deleteTodoForm)
+        .addEventListener('submit', UI.deleteTodoHandler);
+
+      // Add event listener for the toggle completed todo form
+      document.querySelector(DOMElements.toggleCompletedTodoForm)
+        .addEventListener('submit', UI.toggleCompletedHandler)
+
+      // Add event listener for the toggle all button
+      document.querySelector(DOMElements.buttonToggleAll)
+        .addEventListener('click', UI.toggleAllHandler);
+    }
+
+  }
+})();
 
 // Init app
 App.init();
